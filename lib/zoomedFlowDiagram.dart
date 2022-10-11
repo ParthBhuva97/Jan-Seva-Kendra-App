1e694c856a05f1e1db2fb5a2097ec05c191919bc
ShabadVaswani <72546893+ShabadVaswani@users.noreply.github.com>
2022-10-11 23:18:49 +0530

    Update zoomedFlowDiagram.dart

--- lib/zoomedFlowDiagram.dart
+++ lib/zoomedFlowDiagram.dart
@@ -12,50 +12,47 @@ class ZoomedFlow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.arrow_back_ios_new_outlined),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
-          ),
-
-          title: const Text(
-            "Flow Diagram",
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-          iconTheme: const IconThemeData(
+      appBar: AppBar(
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.arrow_back_ios_new_outlined),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+        title: const Text(
+          "Flow Diagram",
+          style: TextStyle(
             color: Colors.black,
           ),
-          //backgroundColor: Colors.white,
         ),
-        body: Container(
-          child: Container(
-            margin: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Container(
-                  height: 150,
+        iconTheme: const IconThemeData(
+          color: Color.fromARGB(183, 0, 0, 0),
+        ),
+        backgroundColor: Color.fromARGB(177, 0, 0, 0),
+        //leading: Image.asset('assets/images/logo.png'),
+      ),
+      body: Container(
+        color: Color.fromARGB(216, 0, 0, 0),
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(
+                height: 150,
+              ),
+              Center(
+                child: PinchZoomReleaseUnzoomWidget(
+                  useOverlay: false,
+                  //fingersRequiredToPinch: -1,
+                  child: Image.asset('assets/images/flow.jpeg'),
                 ),
-                Center(
-                  child: PinchZoomReleaseUnzoomWidget(
-                    useOverlay: false,
-                    //fingersRequiredToPinch: -1,
-                    child: Image.asset('assets/images/flow.jpeg'),
-                  ),
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
